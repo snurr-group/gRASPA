e4ae65e25f413fb1f44d7162bbf5d0d7ae8c2b97
zhaoli2023 <43864970+Zhaoli2042@users.noreply.github.com>
2023-03-08 17:50:49 -0600

    Delete Nov-03-Cleaned directory

--- Nov-03-Cleaned/RN.cpp
+++
@@ -1,9 +0,0 @@
-#include "RN.h"
-#include <cstdlib>
-#include <random>
-double get_random_from_zero_to_one()
-{
-  //return (double) (rand()/RAND_MAX);
-  //std::srand(1.0);
-  return static_cast<double>(std::rand()) / RAND_MAX;
-}
--- Nov-03-Cleaned/mc_insertion_deletion.cu
+++
@@ -1,8 +0,0 @@
-#include "mc_insertion_deletion.h"
-#include "RN.h"
-#include "GPU_Reduction.cuh"
-//#include "data_struct.h"
-#include <vector>
-#include "VDW_Coulomb.cuh"
-#include "mc_translation.h"
-//#include "mc_widom.h"
