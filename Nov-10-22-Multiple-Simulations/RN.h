c01feb061bd7b97100df6aa2351efadbe86bb983
zhaoli2023 <43864970+Zhaoli2042@users.noreply.github.com>
2023-03-08 17:40:06 -0600

    Delete Nov-10-22-Multiple-Simulations directory

--- Nov-10-22-Multiple-Simulations/RN.h
+++
@@ -1,4 +0,0 @@
-#include <cstdlib>
-#include <random>
-using namespace std;
-double get_random_from_zero_to_one();
--- Nov-10-22-Multiple-Simulations/axpy.h
+++
@@ -1,7 +0,0 @@
-#include "VDW_Coulomb.cuh"
-//#include "Pairwise_Energy_Diff.cuh"
-#include "GPU_alloc.cuh"
-#include "RN.h"
-#include <vector>
-//#include "read_data.h"
-double Run_Simulation(int Cycles, Components& SystemComponents, Boxsize Box, Simulations Sims, ForceField FF, RandomNumber Random, WidomStruct Widom, Units Constants, double init_energy, bool DualPrecision, std::vector<size_t>& NumberOfCreateMolecules, bool CreateMolecules);
